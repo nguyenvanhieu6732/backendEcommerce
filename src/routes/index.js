7b101f263f7433baeb97b215b3e9796ef7a927bb
HieeusNguyen <hieudeptrai4603@gmail.com>
2024-12-12 22:47:49 +0700

    Merge code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,13 @@
 const UserRouter = require("./UserRouter")
 const ProductRouter = require("./ProductRouter")
-<<<<<<< HEAD
 const CheckOutRouter = require("./CheckOutRouter")
-=======
 const OrderRouter = require("./OrderRouter")
->>>>>>> 971581d4f4b83c90cdd255b8d512f163224d3ea1
 
 const routes = (app) => {
     app.use("/api/user", UserRouter)
     app.use("/api/product", ProductRouter)
-<<<<<<< HEAD
     app.use("/checkout", CheckOutRouter)
-=======
     app.use("/api/order", OrderRouter)
->>>>>>> 971581d4f4b83c90cdd255b8d512f163224d3ea1
 }
 
 module.exports = routes
